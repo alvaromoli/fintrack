448da865ecb2363a72da27e341927eed370a150e
Álvaro Molina <abusmolina@gmail.com>
2019-05-22 21:21:06 -0400

    ADD UI registration users, ADD first_name and last_name to Devise, ADD logout nav, FIX nav in layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   root "welcome#index"
   get 'portfolio', to: "users#portfolio"
   get 'stocks/search', to: "stocks#search"
